139e007174146818261488799f835ace0b52d5c9
Oleg <123628903+ji-ki@users.noreply.github.com>
2023-09-12 14:47:22 +0300

    Add files via upload

---
+++ osmmaps/src/main/java/ru/mirea/feofanov/osmmaps/MainActivity.java
@@ -0,0 +1,117 @@
+package ru.mirea.feofanov.osmmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.PreferenceManager;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import ru.mirea.feofanov.osmmaps.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mapView = null;
+    private ActivityMainBinding binding;
+    private boolean isWork = false;
+    private static final int REQUEST_CODE_PERMISSION = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        int positionPermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        int positionPermissionStatus2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (positionPermissionStatus == PackageManager.PERMISSION_GRANTED
+                && positionPermissionStatus2 == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+            // Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_PERMISSION);
+        }
+        setContentView(binding.getRoot());
+        mapView = binding.mapView;
+        mapView.setZoomRounding(true);
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(15.0);
+        GeoPoint startPoint = new GeoPoint(55.794229, 37.700772);
+        mapController.setCenter(startPoint);
+        MyLocationNewOverlay locationNewOverlay = new MyLocationNewOverlay(new
+                GpsMyLocationProvider(getApplicationContext()), mapView);
+        locationNewOverlay.enableMyLocation();
+        mapView.getOverlays().add(locationNewOverlay);
+        CompassOverlay compassOverlay = new CompassOverlay(getApplicationContext(), new
+                InternalCompassOrientationProvider(getApplicationContext()), mapView);
+        compassOverlay.enableCompass();
+        mapView.getOverlays().add(compassOverlay);
+        final Context context = this.getApplicationContext();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(mapView);
+        scaleBarOverlay.setCentred(true);
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        mapView.getOverlays().add(scaleBarOverlay);
+        drawIcon("МИРЭА - Российский техногический университет",
+                R.drawable.university, 55.794229, 37.700772);
+        mapView.getOverlays().add(scaleBarOverlay);
+        drawIcon("Центральный парк культуры и отдыха им. М.Горькогo",
+                R.drawable.park_icon, 55.730115, 37.603232);
+
+    }
+    public void drawIcon(String text, Integer icon, Double aLatitude, Double aLongitude){
+        Marker marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(aLatitude, aLongitude));
+        marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(),text,
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker);
+
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(),
+                icon, null));
+
+        marker.setTitle("Title");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+}
