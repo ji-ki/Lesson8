27725a64d8b98cd58a107590bb26ea2867595478
Oleg <123628903+ji-ki@users.noreply.github.com>
2023-09-12 14:48:09 +0300

    Add files via upload

---
+++ yandexmaps/src/main/java/ru/mirea/feofanov/yandexmaps/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.feofanov.yandexmaps;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "b6e114f4-0f35-4c25-99cd-d061b40b3492";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
